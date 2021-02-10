6506e2c1b6e522241700513f3e1c4ce1fcd8a343
rawilder95 <rawilder@syr.edu>
2021-02-09 22:40:45 -0800

    some changes to the main R script.  I was trying to add a distribution line over the histogram, but will go over that with you tomorrow.

--- MATLAB_data.R
+++ MATLAB_data.R
@@ -1,5 +1,5 @@
 dir <- getwd()
-target_dir <- "/Users/rebeccawilder/Desktop/CMR"
+target_dir <- "/Users/rebeccawilder/First-Year-Project"
 setwd(target_dir)
 
 my_data <- read.csv(file= "R_data_02072021.csv")
@@ -17,16 +17,32 @@ ffr= my_data$ffr
 op<- op[1:length(rt)]
 
 lag= my_data$lag
-ffr_out_put= op[ffr==TRUE]
+ffr_output= op[ffr==TRUE]
 #plot hist for rt
 hist(rt,breaks = 100,probability = TRUE, main= "Histogram of Response Times")
-lines(
+
+
 
 #plot hist for lag
 hist(lag, breaks= 100, probability= TRUE, xlab= "N Intervening Items", main= "Intervening Items Between Presentation and IFR")
 
 #plot hist for 'was it ffr?'
-hist(ffr_out_put, breaks= 100, probability= TRUE, main= "IFR Output Positions That Were FFR", xlab= "Output")
+ffr_hist= hist(ffr_output, density= 20, breaks= 100, probability= TRUE, main= "IFR Output Positions That Were FFR", xlab= "Output")
+
+
+
+
+#I've been trying to figure out how to do a frequency dist line over the histogram, but I gave up after spending too much time on it. 
+xfit= seq(min(ffr_output), max(ffr_output))
+yfit= dnorm(xfit, mean= mean(ffr_output), sd= sd(ffr_output))
+
+#yfit <- yfit *diff(ffr_hist$mids[1:2]*length(ffr_output))
+lines(xfit, yfit, col= "blue")
+
+
+
+
+
 
 
              
